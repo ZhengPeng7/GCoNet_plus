e7acd30c8b3140cc8599f652ee023871f06729a0
zhengpeng7 <zhengpeng0108@gmail.com>
2021-07-02 22:13:17 +0400

    Add refinement module.

--- models/modules.py
+++ models/modules.py
@@ -430,51 +430,52 @@ class DBHead(nn.Module):
         return y
 
 class RefUnet(nn.Module):
-    def __init__(self,in_ch,inc_ch):
+    # Refinement
+    def __init__(self, in_ch, inc_ch):
         super(RefUnet, self).__init__()
-        self.conv0 = nn.Conv2d(in_ch,inc_ch,3,padding=1)
-        self.conv1 = nn.Conv2d(inc_ch,64,3,padding=1)
+        self.conv0 = nn.Conv2d(in_ch, inc_ch, 3, padding=1)
+        self.conv1 = nn.Conv2d(inc_ch, 64, 3, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu1 = nn.ReLU(inplace=True)
 
-        self.pool1 = nn.MaxPool2d(2,2,ceil_mode=True)
-        self.conv2 = nn.Conv2d(64,64,3,padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2, ceil_mode=True)
+        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
         self.relu2 = nn.ReLU(inplace=True)
 
-        self.pool2 = nn.MaxPool2d(2,2,ceil_mode=True)
-        self.conv3 = nn.Conv2d(64,64,3,padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2, ceil_mode=True)
+        self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
         self.bn3 = nn.BatchNorm2d(64)
         self.relu3 = nn.ReLU(inplace=True)
 
-        self.pool3 = nn.MaxPool2d(2,2,ceil_mode=True)
-        self.conv4 = nn.Conv2d(64,64,3,padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2, ceil_mode=True)
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
         self.bn4 = nn.BatchNorm2d(64)
         self.relu4 = nn.ReLU(inplace=True)
 
-        self.pool4 = nn.MaxPool2d(2,2,ceil_mode=True)
+        self.pool4 = nn.MaxPool2d(2, 2, ceil_mode=True)
         #####
-        self.conv5 = nn.Conv2d(64,64,3,padding=1)
+        self.conv5 = nn.Conv2d(64, 64, 3, padding=1)
         self.bn5 = nn.BatchNorm2d(64)
         self.relu5 = nn.ReLU(inplace=True)
         #####
-        self.conv_d4 = nn.Conv2d(128,64,3,padding=1)
+        self.conv_d4 = nn.Conv2d(128, 64, 3, padding=1)
         self.bn_d4 = nn.BatchNorm2d(64)
         self.relu_d4 = nn.ReLU(inplace=True)
 
-        self.conv_d3 = nn.Conv2d(128,64,3,padding=1)
+        self.conv_d3 = nn.Conv2d(128, 64, 3, padding=1)
         self.bn_d3 = nn.BatchNorm2d(64)
         self.relu_d3 = nn.ReLU(inplace=True)
 
-        self.conv_d2 = nn.Conv2d(128,64,3,padding=1)
+        self.conv_d2 = nn.Conv2d(128, 64, 3, padding=1)
         self.bn_d2 = nn.BatchNorm2d(64)
         self.relu_d2 = nn.ReLU(inplace=True)
 
-        self.conv_d1 = nn.Conv2d(128,64,3,padding=1)
+        self.conv_d1 = nn.Conv2d(128, 64, 3, padding=1)
         self.bn_d1 = nn.BatchNorm2d(64)
         self.relu_d1 = nn.ReLU(inplace=True)
 
-        self.conv_d0 = nn.Conv2d(64,1,3,padding=1)
+        self.conv_d0 = nn.Conv2d(64, 1, 3, padding=1)
 
         self.upscore2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
 
@@ -492,12 +493,12 @@ class RefUnet(nn.Module):
         hx = self.pool4(hx4)
         hx5 = self.relu5(self.bn5(self.conv5(hx)))
         hx = self.upscore2(hx5)
-        d4 = self.relu_d4(self.bn_d4(self.conv_d4(torch.cat((hx,hx4),1))))
+        d4 = self.relu_d4(self.bn_d4(self.conv_d4(torch.cat((hx, hx4), 1))))
         hx = self.upscore2(d4)
-        d3 = self.relu_d3(self.bn_d3(self.conv_d3(torch.cat((hx,hx3),1))))
+        d3 = self.relu_d3(self.bn_d3(self.conv_d3(torch.cat((hx, hx3), 1))))
         hx = self.upscore2(d3)
-        d2 = self.relu_d2(self.bn_d2(self.conv_d2(torch.cat((hx,hx2),1))))
+        d2 = self.relu_d2(self.bn_d2(self.conv_d2(torch.cat((hx, hx2), 1))))
         hx = self.upscore2(d2)
-        d1 = self.relu_d1(self.bn_d1(self.conv_d1(torch.cat((hx,hx1),1))))
+        d1 = self.relu_d1(self.bn_d1(self.conv_d1(torch.cat((hx, hx1), 1))))
         residual = self.conv_d0(d1)
         return x + residual
